dd1926615c1daf23fc8a2e7432d33c3ad93c7451
ARSH <arshrakholiya3@gmail.com>
2026-05-12 23:06:49 +0530

    Created an app

---
+++ firstproject/firstapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    phone = models.CharField(max_length = 15)
+    photo = models.ImageField(upload_to = 'photos')
+    designation = models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+    
+        
