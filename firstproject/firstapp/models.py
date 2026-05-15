6c17e3396cf2c2e2a20dbcd8c023e5b64cab0dee
ARSH <arshrakholiya3@gmail.com>
2026-05-15 21:29:07 +0530

    Fetched data from the database and displayed it on the frontend

--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -10,5 +10,6 @@ class Employee(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     
+    def __str__(self):
+        return self.name
     
-        
